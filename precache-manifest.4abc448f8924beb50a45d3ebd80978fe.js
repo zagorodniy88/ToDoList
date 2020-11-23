d313f82705a13e1179b1e78ca6ae1db1c637ff11
zagorodniy88 <42653583+zagordniy88@users.noreply.github.com>
2020-11-23 16:16:07 -0400

    Updates

---
+++ precache-manifest.4abc448f8924beb50a45d3ebd80978fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "331754423b82df9e8a9689568ecefea2",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "9c0ad1af8509b2f1c68c",
+    "url": "/ToDoList/static/css/2.1afe660f.chunk.css"
+  },
+  {
+    "revision": "3705e49e7277c4f393b0",
+    "url": "/ToDoList/static/css/main.5b320abd.chunk.css"
+  },
+  {
+    "revision": "9c0ad1af8509b2f1c68c",
+    "url": "/ToDoList/static/js/2.1f15b3c8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ToDoList/static/js/2.1f15b3c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3705e49e7277c4f393b0",
+    "url": "/ToDoList/static/js/main.f5f9ca83.chunk.js"
+  },
+  {
+    "revision": "836b70caa36cf1f7efa4",
+    "url": "/ToDoList/static/js/runtime-main.4e56d59b.js"
+  }
+]);
